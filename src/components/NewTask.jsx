bc095b093449a76de997f339f6b4b609f01bb77f
TiagoToyosato <toliveira_3@hotmail.com>
2024-01-25 17:50:39 +0000

    Add Task

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
-function NewTask() {
+function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    onAdd(enteredTask);
+    setEnteredTask("");
+  }
+
   return (
     <div className="flex items-center gap-4">
-      <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-      <button className="text-stone-700 hover:text-stone-900">Add Task</button>
+      <input
+        onChange={handleChange}
+        value={enteredTask}
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+      />
+      <button
+        onClick={handleClick}
+        className="text-stone-700 hover:text-stone-900"
+      >
+        Add Task
+      </button>
     </div>
   );
 }
